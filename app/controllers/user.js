13423d531764a782cc08a982a794da1b92544f3f
Sylvestre <sbouchot@enssat.fr>
2016-09-17 15:33:45 +0200

    Add encryption

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,17 +2,28 @@ var express 		= require('express'),
 	mongoose 		= require('mongoose'),
 	UserModel 		= mongoose.model('User'),
 	jwt 			= require('jsonwebtoken'),
-	secretConfig	= require('../../config/config');
+	secretConfig	= require('../../config/config'),
+	bcrypt			= require('bcrypt');
 
+var salt = bcrypt.genSaltSync(10);
 var User = {
 	create: function(req, res, next){
 		var fields = ['username', 'password', 'email', 'phonenumber'];
 		for(var i = 0; i < fields.length; i++) {
 			if(!req.body[fields[i]]) return res.status(404).json({message: fields[i], error: 'Missing'})
 		}
-		UserModel.create({nickname: req.body.username, password: req.body.password, email: req.body.email, phonenumber: req.body.phonenumber}).then(function(user){
-			return res.status(200).json(user);
-		});
+		bcrypt.genSalt(10, function (err, salt) {
+			if(err) return res.status(500).json({message: err.message, error: err});
+
+			bcrypt.hash(req.body.password, salt, function (err, hash) {
+				if(err) return res.status(500).json({message: err.message, error: err});
+
+				UserModel.create({nickname: req.body.username, password: hash, email: req.body.email, phonenumber: req.body.phonenumber}).then(function(user){
+					return res.status(200).json(user);
+				});
+			});
+      	});
+
 	},
 	login: function(req, res, next){
 		if(!req.body.username) return res.status(404).json({message: 'Username', error: 'Missing'});
@@ -20,7 +31,7 @@ var User = {
 
 		UserModel.findOne({nickname: req.body.username}).then(function(user){
 			if(!user) return res.json({message : 'Login failed', error: 'Error'});
-			if(user.password != req.body.password) return res.json({message : 'Login failed', error: 'Error'});
+			if(!bcrypt.compareSync(req.body.password, user.password)) return res.json({message : 'Login failed', error: 'Error'});
 
 			var token = jwt.sign(user, secretConfig.tokenSalt, {
 				expiresIn: '1440m'
