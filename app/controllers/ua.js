f067fa59651a1488d3d9cd317bad95887f4390ca
Sylvestre <sbouchot@enssat.fr>
2016-09-23 15:35:19 +0200

    Fix

--- app/controllers/ua.js
+++ app/controllers/ua.js
@@ -7,9 +7,9 @@ var Ua = {
 	create: function(req, res, next){
 		var fields = ['place', 'description'];
 		for(var i = 0; i < fields.length; i++) {
-			if(!req.body[fields[i]]) return res.error({message: fields[i], error: 'Missing'})
+			if(!req.body[fields[i]]) return res.error({message: fields[i], error: 'Missing'});
 		}
-		UaModel.create({place: req.body.place, description: req.body.description, user: req.body.user, deleted: false, owner: req.user._id}).then(function(ua){
+		UaModel.create({place: req.body.place, description: req.body.description, deleted: false, owner: req.user._id}).then(function(ua){
 			UserModel.findOne({_id: req.user._id}).then(function(user){
 				user.uas.push(ua._id);
 				user.save();
@@ -20,7 +20,7 @@ var Ua = {
 
 	get: function(req, res, next){
 		UaModel.findOne({_id: req.params.id}).then(function(ua){
-			if(!ua || ua.deleted) return res.error({message: 'Ua does not exist', error: 'Not found'})
+			if(!ua || ua.deleted) return res.error({message: 'Ua does not exist', error: 'Not found'});
 			return res.ok(ua);
 		}).catch(function(err){
 			return res.error({message: 'Ua not found', error: 'Not found'});
@@ -29,7 +29,7 @@ var Ua = {
 
 	delete: function(req, res, next){
 		UaModel.findOne({_id: req.params.id}).then(function(ua){
-			if(!ua) return res.error({message: 'Ua does not exist', error: 'Not found'})
+			if(!ua) return res.error({message: 'Ua does not exist', error: 'Not found'});
 			if(ua.deleted) return res.error({message: 'Already done'});
 			UaModel.update({_id: ua.id}, {deleted: true}).then(function(data){
 				return res.ok({message: 'OK'});
