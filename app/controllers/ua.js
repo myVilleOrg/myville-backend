5d14250ad43c83f3e2389778ba3c9584cf308209
Emeric <emeric.bourillon@gmail.com>
2016-09-23 14:29:30 +0200

    uas created

--- app/controllers/ua.js
+++ app/controllers/ua.js
@@ -6,7 +6,6 @@ var Ua = {
 	create: function(req, res, next){
 		var fields = ['place', 'description', 'user'];
 		for(var i = 0; i < fields.length; i++) {
-			console.log(req.body[fields[i]]);
 			if(!req.body[fields[i]]) return res.error({message: fields[i], error: 'Missing'})
 		}
 		UaModel.create({place: req.body.place, description: req.body.description, user: req.body.user, deleted: false}).then(function(ua){
@@ -16,6 +15,7 @@ var Ua = {
 
 	get: function(req, res, next){
 		UaModel.findOne({_id: req.params.id}).then(function(ua){
+			if(!ua || ua.deleted) return res.error({message: 'Ua does not exist', error: 'Not found'})
 			return res.status(200).json(ua);
 		}).catch(function(err){
 			return res.error({message: 'Ua not found', error: 'Not found'});
@@ -24,6 +24,7 @@ var Ua = {
 
 	delete: function(req, res, next){
 		UaModel.findOne({_id: req.params.id}).then(function(ua){
+			if(!ua) return res.error({message: 'Ua does not exist', error: 'Not found'})
 			if(ua.deleted) return res.error({message: 'Already done'});
 			UaModel.update({_id: ua.id}, {deleted: true}).then(function(data){
 				return res.ok({message: 'OK'});
