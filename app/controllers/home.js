924c6ecb294574a4cbbc71eba6f6de5a0eac02fc
Sylvestre <sbouchot@enssat.fr>
2016-09-23 10:34:25 +0200

    Last fix ident

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-		router 	= express.Router();
+	router 	= express.Router();
 
 router.get('/', function (req, res, next) {
 	return res.status(200).json({message: 'Welcome on API'});
