3533abf7a73b5f41396db80bc65370781e208d13
Sylvestre <sbouchot@enssat.fr>
2017-02-03 10:24:16 +0100

    Remove useless code to get count number

--- app/controllers/vote.js
+++ app/controllers/vote.js
@@ -1,21 +1,16 @@
 var express 		= require('express'),
 	mongoose 		= require('mongoose'),
-	UaModel 		= mongoose.model('Ua'),
-	UserModel 		= mongoose.model('User'),
 	VoteModel		= mongoose.model('Vote');
 
 var Vote = {
 	getVoteByUa: function(req, res, next){
 		VoteModel.findOne({user: req.user._id, ua: req.params.uaId}).then(function(vote){
-			if(!vote) return res.error({count: 0});
-			VoteModel.count({ua: req.params.uaId}).then(function(countVote){
-				return res.ok({count: countVote, vote:vote.vote});
-			});
+			if(!vote) return res.error({message: 'Not voted'});
+			return res.ok({vote:vote.vote});
 		}).catch(function(err){
 			return res.error(err);
 		});
 	}
-
 };
 module.exports = function (app) {
 	app.get('/vote/:uaId',	Vote.getVoteByUa);
