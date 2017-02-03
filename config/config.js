05c33b19f5225aebaecbd6d511c51ea9ab0c5f4b
Sylvestre <sbouchot@enssat.fr>
2017-02-03 15:48:11 +0100

    Add clean config file

---
+++ config/config.js
@@ -0,0 +1,54 @@
+var path = require('path'),
+		rootPath = path.normalize(__dirname + '/..'),
+		env = process.env.NODE_ENV || 'development';
+
+var config = {
+	development: {
+		root: rootPath,
+		app: {
+			name: 'backend'
+		},
+		port: process.env.PORT || 3000,
+		db: 'mongodb://localhost/backend-development',
+		tokenSalt: 'zbeb',
+		nosecurePath: ['/user/reset', '/user/create', '/user/login', '/', '/user/login/facebook', '/user/login/google', '/ua/get/geo', '/ua/get/popular', '/user/forgetPassword'],
+		facebook: {
+			appId: '',
+			appSecret: ''
+		},
+		google: {
+			appId: '',
+			appSecret: ''
+		},
+		email: {
+			host: 'smtp.gmail.com',
+			port: 587,
+			auth: {
+				user: '',
+				pass: ''
+			}
+		}
+	},
+
+	test: {
+		root: rootPath,
+		app: {
+			name: 'backend'
+		},
+		port: process.env.PORT || 3000,
+		db: 'mongodb://localhost/backend-test',
+		tokenSalt: 'zbeb'
+	},
+
+	production: {
+		root: rootPath,
+		app: {
+			name: 'backend'
+		},
+		port: process.env.PORT || 3000,
+		db: 'mongodb://localhost/backend-production',
+		tokenSalt: 'zbeb'
+	},
+};
+
+module.exports = config[env];
