2fd658c0a794adb54a793e6dfa41c12eaf7d155e
ThiRossi <trossign@enssat.fr>
2018-02-16 15:32:50 +0100

    fix some problems

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -237,7 +237,7 @@ var User = {
 						});
 					});
 				} else {
-					return res.error({message: 'Bad old password'});
+					return res.error({message: 'Ancien mot de passe non valide.'});
 				}
 			}
 		}).catch(function(err){
